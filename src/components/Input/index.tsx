74570994b146ad40e84ce000626f124e2bf39ac2
Felizardo <devfelizardo@gmail.com>
2020-12-19 17:23:15 -0300

    adding error icon when there is an invalid input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
 import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from "react";
 import { IconBaseProps } from "react-icons";
+import { FiAlertCircle } from "react-icons/fi";
 import { useField } from "@unform/core";
 
 import { Container } from "./styles";
@@ -38,7 +39,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     <Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
       {Icon && <Icon size={20} />}
       <input onFocus={handleInputFocus} onBlur={handleInputBlur} defaultValue={defaultValue} ref={inputRef} {...rest} />
-      {error}
+      {error && <FiAlertCircle color="c53030" size={20} />}
     </Container>
   );
 };
