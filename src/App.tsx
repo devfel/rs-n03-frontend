51ef2e2eb8f978b8bc1de54dae822ec741d8f7ff
Felizardo <devfelizardo@gmail.com>
2020-12-20 21:53:21 -0300

    starting the context api for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import AuthContext from "./context/AuthContext";
 
-import SignIn from "./pages/SignIn";
 //import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 import GlobalStyle from "./styles/global";
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <SignIn />
+    <AuthContext.Provider value={{ name: "Luiz" }}>
+      <SignIn />
+    </AuthContext.Provider>
   </>
 );
 
