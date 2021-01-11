db3c37219e96ea11ef295336def79e5fb6cbbd6e
Felizardo <devfelizardo@gmail.com>
2021-01-11 10:31:45 -0300

    changing routes index and tsx files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import { Switch } from "react-router-dom";
+import React from 'react';
+import { Switch } from 'react-router-dom';
 
-import Route from "./Route";
+import Route from './Route';
 
-import SignIn from "../pages/SignIn";
-import SignUp from "../pages/SignUp";
-import Dashboard from "../pages/Dashboard";
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
-const Routes: React.FC = () => {
-  return (
-    <Switch>
-      <Route path="/" exact component={SignIn} />
-      <Route path="/signup" component={SignUp} />
+import Profile from '../pages/Profile';
+import Dashboard from '../pages/Dashboard';
 
-      <Route path="/dashboard" component={Dashboard} isPrivate />
-    </Switch>
-  );
-};
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
+
+    <Route path="/profile" component={Profile} isPrivate />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
+  </Switch>
+);
 
 export default Routes;
